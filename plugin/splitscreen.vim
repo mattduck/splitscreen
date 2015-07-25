fb88091403b8bd8ab483ec2b6e88ecde213e7772
Matthew Duck <matt@mattduck.com>
2015-07-25 15:26:54 +0100

    Update keybindings
    - Remove tmux vim navigator, because it moves the cursor out of vim when doing
      eg. "C-w h" on the leftmost vim split. The main advantage of this setup is
      that you get tmux-like bindings for navigating vim splits, but splitscreen
      already provides this.
    
    - Use elscreen to provide tabs in emacs
    
    - Remove select-layout-tiled from tmux as the <Space> prefix, because it changes
      the layout of panes, unlike the "balance" commands in vim/emacs.

--- plugin/splitscreen.vim
+++ plugin/splitscreen.vim
@@ -4,6 +4,7 @@ nnoremap <C-w>c :tabnew<CR>
 nnoremap <C-w>n :tabnext<CR>
 nnoremap <C-w>p :tabprevious<CR>
 nnoremap <C-w>! :tabedit %<CR>
+nnoremap <C-w>X :tabclose
 
 nnoremap <C-w>% :vsplit<CR>
 nnoremap <C-w>" :split<CR>
@@ -15,10 +16,12 @@ nnoremap <C-w><C-k> <C-w>+
 nnoremap <C-w><C-l> <C-w>>
 nnoremap <C-w><Space> <C-w>=
 
-" vim tmux navigator
-let g:tmux_navigator_no_mappings = 1
-nnoremap <silent> <C-w>h :TmuxNavigateLeft<CR>
-nnoremap <silent> <C-w>j :TmuxNavigateDown<CR>
-nnoremap <silent> <C-w>k :TmuxNavigateUp<CR>
-nnoremap <silent> <C-w>l :TmuxNavigateRight<CR>
-"nnoremap <silent> {Previous-Mapping} :TmuxNavigatePrevious<CR>
+nnoremap <C-w>1 1gt
+nnoremap <C-w>2 2gt
+nnoremap <C-w>3 3gt
+nnoremap <C-w>4 4gt
+nnoremap <C-w>5 5gt
+nnoremap <C-w>6 6gt
+nnoremap <C-w>7 7gt
+nnoremap <C-w>8 8gt
+nnoremap <C-w>9 9gt
--- splitscreen.el
+++ splitscreen.el
@@ -2,30 +2,47 @@
 ;; URL: https://github.com/mattduck/splitscreen
 
 (require 'evil)
+(require 'elscreen)
 
 ;; Splitscreen mode will just provide a keymap.
 (defvar ss-mode-map (make-sparse-keymap))
 (define-prefix-command 'ss-prefix)
 (define-key ss-mode-map (kbd "C-w") 'ss-prefix)
 
+;; We override these. Just declare them as part of the splitscreen map, not
+;; evil-window-map.
 (define-key evil-window-map (kbd "n") nil)
-(define-key ss-prefix (kbd "n") 'next-buffer)
 (define-key evil-window-map (kbd "p") nil)
-(define-key ss-prefix (kbd "p") 'previous-buffer)
+(define-key evil-window-map (kbd "c") nil)
+(define-key evil-window-map (kbd "C-h") nil)
+(define-key evil-window-map (kbd "C-j") nil)
+(define-key evil-window-map (kbd "C-k") nil)
+(define-key evil-window-map (kbd "C-l") nil)
+(define-key evil-window-map (kbd "C-l") nil)
 
+(define-key ss-prefix (kbd "c") 'elscreen-create)
+(define-key ss-prefix (kbd "n") 'elscreen-next)
+(define-key ss-prefix (kbd "p") 'elscreen-previous)
+(define-key ss-prefix (kbd "X") 'elscreen-kill)
 (define-key ss-prefix (kbd "%") 'split-window-right)
 (define-key ss-prefix (kbd "\"") 'split-window-below)
 (define-key ss-prefix (kbd "x") 'delete-window)
 (define-key ss-prefix (kbd "z") 'delete-other-windows)
-(define-key evil-window-map (kbd "C-h") nil)
 (define-key ss-prefix (kbd "C-h") 'evil-window-decrease-width)
-(define-key evil-window-map (kbd "C-j") nil)
 (define-key ss-prefix (kbd "C-j") 'evil-window-decrease-height)
-(define-key evil-window-map (kbd "C-k") nil)
 (define-key ss-prefix (kbd "C-k") 'evil-window-increase-height)
-(define-key evil-window-map (kbd "C-l") nil)
 (define-key ss-prefix (kbd "C-l") 'evil-window-increase-width)
 (define-key ss-prefix (kbd "SPC") 'balance-windows)
+(define-key ss-prefix (kbd "0") '(lambda() (interactive) (elscreen-goto 0)))
+(define-key ss-prefix (kbd "1") '(lambda() (interactive) (elscreen-goto 1)))
+(define-key ss-prefix (kbd "2") '(lambda() (interactive) (elscreen-goto 2)))
+(define-key ss-prefix (kbd "3") '(lambda() (interactive) (elscreen-goto 3)))
+(define-key ss-prefix (kbd "4") '(lambda() (interactive) (elscreen-goto 4)))
+(define-key ss-prefix (kbd "5") '(lambda() (interactive) (elscreen-goto 5)))
+(define-key ss-prefix (kbd "6") '(lambda() (interactive) (elscreen-goto 6)))
+(define-key ss-prefix (kbd "7") '(lambda() (interactive) (elscreen-goto 7)))
+(define-key ss-prefix (kbd "8") '(lambda() (interactive) (elscreen-goto 8)))
+(define-key ss-prefix (kbd "9") '(lambda() (interactive) (elscreen-goto 9)))
 
 (define-minor-mode splitscreen-mode
     "Provides tmux-like bindings for managing windows and buffers. 
@@ -34,5 +51,7 @@
     :lighter " Split"
     :global 1
     :keymap ss-mode-map)
+
+(elscreen-start)
 
 (provide 'splitscreen-mode)
